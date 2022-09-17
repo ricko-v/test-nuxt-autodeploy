394ce9fb282f5540cdbf8c949760ac9195d0ad18
ricko-v <ricko-v@users.noreply.github.com>
2022-09-17 08:11:57 +0000

    deploy: 71c8d0ff5f80fc9b46ea4caac2e3527dd25edb6c

--- _nuxt/static/1663402220/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-nuxt-autodeploy\u002F_nuxt\u002Fstatic\u002F1663402220",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest-nuxt-autodeploy\u002F",assetsPath:"\u002Ftest-nuxt-autodeploy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
