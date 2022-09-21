2e4b373885f6c8aa57f291070da4494a4b4b497f
ricko-v <ricko-v@users.noreply.github.com>
2022-09-21 08:17:52 +0000

    deploy: 381028970493d887a2ba3ab193cd2c6ca2c4ec6e

--- _nuxt/5ef44d4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{255:function(n,t,e){"use strict";e.r(t);var l=e(43),component=Object(l.a)({},(function(){return(0,this._self._c)("p",[this._v("\n  Test\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ _nuxt/static/1663748272/tes/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-nuxt-autodeploy\u002F_nuxt\u002Fstatic\u002F1663748272",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftes",config:{_app:{basePath:"\u002Ftest-nuxt-autodeploy\u002F",assetsPath:"\u002Ftest-nuxt-autodeploy\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
